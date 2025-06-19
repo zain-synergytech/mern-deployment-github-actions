c73dc13d9c01c14d10a669c53c608088942c50d7
zainulhassan360 <zainulhassan@360synergytech.com>
2025-06-19 17:43:30 +0500

    Update CORS. Allow all origins.

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 // Routes
 app.use('/api/users', usersRouter);
