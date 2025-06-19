6d30582d5000b56074bd15800a37ad9c920246e6
zainulhassan360 <zainulhassan@360synergytech.com>
2025-06-19 22:35:16 +0500

    Update backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,7 @@ const generateUsers = (count = 5) =>
     id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    avatar: faker.image.personPortrait(),
+    avatar: faker.image.avatarGitHub(),
   }));
 
 router.get('/', (_req, res) => {
